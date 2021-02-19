3592ea7df655f15298a2bc57b59b3d72f223bee6
Alpha-maker <70609884+Alpha-maker@users.noreply.github.com>
2021-02-18 19:26:38 -0500

    Add files via upload

--- Tree/BST.java
+++ Tree/BST.java
@@ -5,8 +5,20 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-
+/**
+ * @since 2021. DETAILS.....
+ * a generic (any datatype K - like another comparable data structure like an array) 
+ * Binary Search Tree for searching, computational data storage
+ * and an add-on of collecting the data if needed into a list.
+ * It's collection is iterable. 
+ * https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
+ * @version 1.0
+ * @author David Nathaniel (Ambassador of Christ)
+ */
+ 
 public class BST<K extends Comparable <K>> implements Iterable<K> {
+	/**
+	 */
 
 	private class Node {
 		private K data;
@@ -14,22 +26,36 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 		private Node right;
 		private Node(K data) {
 			this.data = data;
-			left = null;
-			right = null;
+			left = right = null;
 		}
 	}
-
+	/**
+	 * @param root of the tree
+	 * @param size
+	 * All methods (insert, search, collection, traversals, and deletion)
+	 * are implemented using recursion. All are in runtime of O(h) where h is the height of the tree.
+	 */
 	private Node root;
 	private int size = 0;
 
+	/**
+	 * Default Constructor. Root is empty.
+	 */
 	public BST() {
 		root = null;
 	}
 
+
+	/**
+	 *	A copy constructor. points to an exist BST object
+	 *	for collecting data over that BST or iterating over
+	 *	the collection of data. 
+	 *	@param root -- root of the BST
+	 */
 	public BST(Node root) {
 		this.root = root;
 	}
-	// O(log(n)) recursive insert algorithm by comparing the keys
+	// O(h) where h is the height of the tree. recursive insert algorithm by comparing the keys
 	private Node insertHelper(Node root, K data) {
 		if (root == null) {
 			size++;
@@ -42,13 +68,18 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 		return root;
 	}
 
-	// Using an helper method to insert a data into the Tree.
+	/**Insert a data into the BinarySearchTree.
+	 * Pass in old root to add new data into it.
+	 * Now reassign root to its modified form
+	 * @param data -- generic (any datatype) in the BST
+	 * @return a BST
+	 */
 	public BST<K> insert(K data){
-		root = insertHelper(root, data); // pass in old root to add new node into it, now reassign root to its modified form
+		root = insertHelper(root, data);
 		return this;
 	}
 
-	// O(log(n)) recursive search algorithm by comparing the keys
+	// O(h) recursive search algorithm by comparing the keys
 	private boolean searchHelper(Node root, K data) {
 		if (root == null) {
 			return false;
@@ -61,35 +92,25 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 		}
 	}
 
-	// Using an helper method to search a data into the Tree.
+	/**search if a data is in the BST.
+	 * returns true if present or false otherwise.
+	 * @param data -- generic (any datatype) in the BST
+	 * @return true or false
+	 */
 	public boolean search(K data){
 		return searchHelper(root, data);
 	}
 
-	// catches and returns the max key in the present subTree
-	// 
+	// catches and returns the max data in the present subTree
 	private K max(Node root) {
-		K result = null;
-		if (root.right != null) {
-			max(root.right);
-		}else {
-			result = root.data;
-		}
-		return result;
+		return root.right != null ? max(root.right) : root.data;
 	}
 
-	// catches and returns the min key in the present subTree
+	// catches and returns the min data in the present subTree
 	private K min(Node root) {
-		K result = null;
-		if (root.left != null) {
-			min(root.left);
-		}else {
-			result = root.data;
-		}
-		return result;
+		return root.left != null ? min(root.left) : root.data;
 	}
 
-	// https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
 	// This one has a much simpler way to delete a data
 	private Node deleteHelper(Node root, K data) {
 		int compare = data.compareTo(root.data);
@@ -97,23 +118,25 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			root.left = deleteHelper(root.left, data);
 		}else if (compare > 0) {
 			root.right = deleteHelper(root.right, data);
-		}else {
+		}else { // else you found the data to be deleted.
 			if (root.left == null && root.right == null) {
 				root = null;
 				size--;
-			}else if (root.left == null && root.right != null) {
+			}else if (root.left == null) {
 				/*K minRight = null;
 				minRight = min(root.right);
 				root.data = minRight;
 				root.right = deleteHelper(root.right, minRight);*/
+				size--;
 				return root.right; // both this preferred strategy and the one on top works
-			}else if (root.left != null && root.right == null) {
+			}else if (root.right == null) {
 				/*K maxLeft = null;
 				maxLeft = max(root.left);
 				root.data = maxLeft;
 				root.left = deleteHelper(root.left, maxLeft);*/
+				size--;
 				return root.left; // both this strategy and the one on top works
-			}else { // I could have used 1 else if and 1 else, but this is for clarity
+			}else { 
 				K maxLeft = null;
 				maxLeft = max(root.left);
 				root.data = maxLeft;
@@ -123,6 +146,12 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 		return root;
 	}
 
+/** Removes a data that is not needed
+ * 	The data is comparable
+ * @param data -- generic (any datatype) in the BST
+ * 
+ * @return a BST
+ */
 	public BST<K> delete(K data){
 		if (this.search(data)) {
 			root = deleteHelper(root, data);
@@ -131,7 +160,10 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			return this;
 		}
 	}
-
+/**
+ * Size of the BST
+ * @return integer (the number of nodes in the BST).
+ */
 	public int size() {
 		return size;
 	}
@@ -143,7 +175,12 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			addKeysToCollection(c, root.right);
 		}
 	}
-
+	
+/**Collects the data in the BST.
+ * Uses a recursive method to get all data.
+ * @return an ArrayList of data in BST.
+ */
+	
 	public ArrayList<K> collection() {
 		ArrayList<K> park = new ArrayList<> ();
 		addKeysToCollection(park, root);
@@ -161,7 +198,11 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			inOrderHelper(root.right);
 		}
 	}
-
+/**
+ * Basic inOrder traversal of a BST
+ * prints them in the natural order.
+ * prints them left, root, and right subtree.
+ */
 	public void inOrder() {
 		inOrderHelper(root);
 		System.out.println();
@@ -174,7 +215,10 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			preOrderHelper(root.right);
 		}
 	}
-
+/**
+* Basic preOrder traversal of a BST
+* prints them root, left, and right subtree.
+**/
 	public void preOrder() {
 		preOrderHelper(root);
 		System.out.println();
@@ -187,12 +231,21 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			System.out.print(root.data + " ");
 		}
 	}
-
+/**
+ * Basic postOrder traversal of a BST
+ * prints them left, right subtree, then the root
+ */
 	public void postOrder() {
 		postOrderHelper(root);
 		System.out.println();
 	}
-
+/**
+ * Select the order that you want the BST to be traversed.
+ * @param in -- (input string)
+ * "in" - for inOrder traversal.
+ * "pre" - for preOrder traversal.
+ * Else a postOrder traversal.
+ */
 	public void selectOrder(String in) {
 		if(in.equals("in")) {
 			this.inOrder();
@@ -215,18 +268,23 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 
 		tree.inOrder();
 
-//		System.out.println(tree.search(80));
-//		System.out.println(tree.size());
+		//		System.out.println(tree.search(80));
+		//		System.out.println(tree.size());
 		System.out.println();
 		tree.delete(70);
-        tree.inOrder();
-        System.out.println(tree.size());
-        System.out.println();
+		tree.inOrder();
+		System.out.println("deleted 70, size is now " +tree.size());
+		
+		tree.delete(60);
+		tree.inOrder();
+		System.out.println("deleted 60, size is now " +tree.size());
+		
+		System.out.println();
 		tree.postOrder();
-//
-//		System.out.println();
-//		tree.preOrder();
-//
+		//
+		//		System.out.println();
+		//		tree.preOrder();
+		//
 		System.out.println();
 		tree.selectOrder("pre");
 		System.out.println();
@@ -236,12 +294,17 @@ public class BST<K extends Comparable <K>> implements Iterable<K> {
 			System.out.print(it.next() + " ");
 		}
 		System.out.println();
-		
+
 		tree.delete(60);
 		tree.selectOrder("in");
 	}
 
-
+/**
+ * Returns an iterator over the collection of BST's data.
+ * hasNext() - If there is more data from the data collection, then return true. Else false.
+ * next() - returns the data.
+ * @throws NoSuchElementException -- cannot remove a nonexisting data.
+ */
 	@Override
 	public Iterator<K> iterator() {
 		// TODO Auto-generated method stub
